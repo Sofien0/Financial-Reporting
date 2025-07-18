dde7752cc35fa458eb34f7d94f423bee64e81bd7
yasminesas <Sassi.yasmine@esprit.tn>
2025-07-18 15:45:14 +0200

    TASK1

---
+++ Extraction&benchmark/clean_invalid.py
@@ -0,0 +1,5 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/yassmine/Extraction&Bechenmark/extract.py
+✅ Nettoyage terminé. 113 fichiers supprimés.
